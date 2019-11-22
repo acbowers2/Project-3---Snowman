ca2802b4a89cec13e42d51251d568a2f42199964
isthatsmv <55513951+isthatsmv@users.noreply.github.com>
2019-11-22 13:11:50 -0600

    Update snowMan.java

--- snowMan.java
+++ snowMan.java
@@ -1,2 +1,67 @@
 public class snowMan{
+   public static void snowMan(String word, char[] empty)
+   {
+      int attempts = 9;
+      String tried = "";
+      Scanner kb = new Scanner(System.in);
+      boolean gameOn = true;
+      boolean won = false;
+      
+      while(gameOn == true)
+      {
+         String winCondition = "";
+         System.out.print("Your current array looks like this :");
+         for(int i = 0; i < empty.length; i++)
+            System.out.print(empty[i]);
+         System.out.println();
+         System.out.println("Please guess a letter: ");
+         String s = kb.nextLine();
+         s = s.toLowerCase();
+         char guess = s.charAt(0);
+         
+         if(s.matches("[0-9.]"))
+            System.out.println("Sorry! Needs to be a letter!");
+         else if(tried.contains(guess + "")){
+               System.out.println("You already tried that one, please input another.");
+            }
+
+         else{
+            if(word.contains(guess + ""))
+            {                 
+               System.out.println("The guessed letter is in the word!");
+               tried += guess + " ";
+               for(int i = 0; i < word.length(); i++){
+                  char hold = word.charAt(i);
+                  if(hold == guess)
+                  {
+                     empty[i] = guess;
+                  } 
+               }
+            }//end contained guess if
+            
+            else
+            {
+               System.out.println("Oh No! That was a bad guess. You now have " + attempts + " attempts remaining.");
+               
+               attempts--;
+               tried += guess + " ";
+               System.out.println();
+               System.out.println("You have tried the followed letters: " + tried);
+               if(attempts == 0)
+                  gameOn = false;
+            }//end bad guess
+            for(int i = 0; i < empty.length; i++)
+            {
+               winCondition += empty[i];
+            }
+            if(!winCondition.contains("-")){
+               gameOn = false;
+               won = true;
+            }
+         }//end guess was letter else
+         System.out.println();
+         System.out.println("~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n");
+
+      }//end while
+      
 }
