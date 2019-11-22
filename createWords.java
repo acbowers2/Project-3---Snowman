b4c3170e70ee68ecf1ed55b5d6023fa8bf6ce55e
acbowers2 <57769898+acbowers2@users.noreply.github.com>
2019-11-22 13:39:42 -0600

    Update createWords.java

--- createWords.java
+++ createWords.java
@@ -1,7 +1,18 @@
-public class createWords{
-    public static void main(String[] args) {
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class CreateWords{
+   
+   private CreateWords()
+   {
+      //private constructor to keep it from creating the objects
+   }
+
+   public static String pullWord()
+   {
       //create the file and the random number to get the word out of the document
-      //create the variables for the chosen word and the empty string
+      //create the variables for the chosen word
       File words = new File("words.txt");
       int choice = (int)(Math.random() * 10);
       String chosen = "";
@@ -14,12 +25,7 @@ public class createWords{
             char[] empty = new char[s.length()];
             if(i == choice){
                chosen = s;
-               
-               for(int j = 0; j < chosen.length(); j++){
-                  empty[j] = '-';
-               }
                i = 10;
-               snowMan(chosen, empty);
             }
             
          }
@@ -27,5 +33,17 @@ public class createWords{
       }catch(FileNotFoundException e){
          System.out.println("Whoopsie! No file there!");
       }
-   }
+      
+      return chosen;
+   }//end pullWord
+   
+   public static char[] makeEmpty(String chosen)
+   {
+      //takes a word and makes an array of dashes that is the same size of the word
+      char[] empty = new char[chosen.length()];
+         for(int j = 0; j < chosen.length(); j++){
+            empty[j] = '-';
+         }
+      return empty;
+   }//end makeEmpty
 }
