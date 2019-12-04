71ce04793ccbb6873467203bae73ad4c2b31853c
acbowers2 <57769898+acbowers2@users.noreply.github.com>
2019-12-03 21:25:27 -0600

    Update snowmanDemo.java

--- snowmanDemo.java
+++ snowmanDemo.java
@@ -3,8 +3,6 @@ public class snowmanDemo
    public static void main(String[] args)
    {
       Player playerOne = new Player();
-      SnowMan yule = new SnowMan();
-      
       playerOne.playGame();
    }
 }
