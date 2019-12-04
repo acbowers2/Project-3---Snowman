a2709ae898c12bb513ca81b3f51782d61540205d
acbowers2 <57769898+acbowers2@users.noreply.github.com>
2019-12-03 21:24:49 -0600

    Update snowMan.java

--- snowMan.java
+++ snowMan.java
@@ -1,38 +1,37 @@
-public class SnowMan extends CreateWords
+public class SnowMan extends CreateWords implements Playable
 {
    private int attempts; 
    private char[] empty;
    private String word;
+   private char guessedLetter;
+   private boolean gameOn = true;
    
    public SnowMan()
    {
       this.word = super.pullWord();
       this.empty = CreateWords.makeEmpty(this.word);
       this.attempts = 9;
-      System.out.print(word);
-      
+      System.out.println("You have 9 attempts to guess the word or the SnowMan will be complete!");
    }
    
    public void playGame()
    {
-      //calls on finds out if it is in the string
-      //if in string it will be sent to correctArray()
-      //if not in the array it will be sent to wrongGuess()
-      char guessed = Player.getGuess(); //WILL HAVE TO CHANGE
-      if(this.word.contains(guessed + ""))
-         this.correctArray(guessed);
+      if(this.word.contains(this.guessedLetter + ""))
+         this.correctArray();
       else
          this.wrongGuess();
+         
+      didWin();
+      didLose();
    }
    
-   public void correctArray(char letter)
+   public void correctArray()
    {
-      //this method runs the for-loop to replace the '-' in the empty[] with the letter at the correct spot
-      //doesn't return anything
+      System.out.println("The guessed letter was in the word!");
       for(int i = 0; i < empty.length; i++)
       {
-         if(this.word.charAt(i) == letter)
-            empty[i] = letter;
+         if(this.word.charAt(i) == guessedLetter)
+            empty[i] = guessedLetter;
       }
    }
    
@@ -40,6 +39,7 @@ public class SnowMan extends CreateWords
    {
       //counts down the attempts
       this.attempts--;
+      System.out.println("Oh No! That was a bad guess. You now have " + attempts + " attempts remaining.");
    }
    
    public boolean didWin()
@@ -55,7 +55,8 @@ public class SnowMan extends CreateWords
       if(didWin == true){
          System.out.println("You have won the game!");
          System.out.println("The word was: " + word);
-         System.out.print("Congratulations! Please play again!");
+         System.out.println("Congratulations! Please play again!");
+         this.gameOn = false;
       }
          
       return didWin;
@@ -69,9 +70,30 @@ public class SnowMan extends CreateWords
       if(didLose == true){
          System.out.println("Oh no... you lost...");
          System.out.println("The word was: " + this.word);
-         System.out.print("Please play again!");
+         System.out.println("Please play again!");
+         System.out.println("      _\n    _| |_\n    ('>')\n >-(     )-< \n  (_______)");
+         this.gameOn = false;
       }
       return didLose;
+   }
+   
+   public boolean getGameOn()
+   {
+      return this.gameOn;
+   }
+   
+   public void setGuessedLetter(char c)
+   {
+      this.guessedLetter = c;
+   }
+   
+   public void printArray()
+   {
+      System.out.println("Your current word looks like: ");
+      for(int i = 0; i < empty.length; i++){
+         System.out.print(empty[i]);
+      }
+      System.out.println();
    }
    
 }//end SnowMan
